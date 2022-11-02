7bebcfa0bd85e9e9f596c0fc757158f651608318
aclloyd17 <113736303+aclloyd17@users.noreply.github.com>
2022-11-02 10:51:02 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'lmuVwBZpdOX8QPRvldNldMc1t',
+  consumer_secret:      'qHGdwd5j7g7ObA7C4ZFVazosshflra2E2V7haMXLgoCLOTKAG7',
+  access_token:         '1584223353698603009-v1Co9uLvAHKlke8Ez1PRglT7C3IH8Y',
+  access_token_secret:  'lgWxZLs7JLLtYLO2cAIEzvC9Bt8ZtORDIjhNRBmSi59dy'
+}
