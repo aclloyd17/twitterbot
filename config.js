566284f145d5b9ac30bf2af878bc8661bd176021
jarizono <113387871+jarizono@users.noreply.github.com>
2022-11-07 17:25:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'lmuVwBZpdOX8QPRvldNldMc1t',
-  consumer_secret:      'qHGdwd5j7g7ObA7C4ZFVazosshflra2E2V7haMXLgoCLOTKAG7',
-  access_token:         '1584223353698603009-v1Co9uLvAHKlke8Ez1PRglT7C3IH8Y',
+  consumer_key:         'TSywypgorgnJgZvBDZKqnLbwv',
+  consumer_secret:      'tRmYdEQwomBFhcAszvCQ5WeADwk2ajgkTpzPcI3KiSqd89lZAQ',
+  access_token:         '1589739532487753735-nSfAFik3UQvaguv3S1OSINheO7big8',
   access_token_secret:  'lgWxZLs7JLLtYLO2cAIEzvC9Bt8ZtORDIjhNRBmSi59dy'
 }
